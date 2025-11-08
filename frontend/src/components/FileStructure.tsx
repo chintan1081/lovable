668af9d1ea8881610c745ea13b7ae63b79e7b8b6
chintan <chintanpatel1081@gmail.com>
2025-11-08 23:13:35 +0530

    file structure ui and preview ui is done

--- frontend/src/components/FileStructure.tsx
+++ frontend/src/components/FileStructure.tsx
@@ -1,43 +1,74 @@
 import { PiTreeStructureBold } from "react-icons/pi";
 import Editor from "@monaco-editor/react";
 import { FiChevronRight } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import { Get, Post } from "@/utils/axios";
+import FolderUI from "./folderui";
 
-const FileStructure = () => {
-    const code = `even programmatic edits
-        minimap: { enabled: false },
-        scrollBeyondLastLine: false,
-      }}
-    />
-  )
-}`
+type FileStructureProp = {
+    projectId: string
+}
+
+const FileStructure = ({ projectId }: FileStructureProp) => {
+    const [fileStructure, setFileStructure] = useState([]);
+    const [filePath, setFilePath] = useState("");
+    const [code, setCode] = useState(``);
+
+    const filepathNav = filePath.split("/").splice(1);
+
+    useEffect(() => {
+        if (!projectId) return;
+        Get(`/api/v0/project/filestructure/${projectId}`)
+            .then((response) => {
+                setFileStructure(response.data.data);
+            })
+    }, [])
+
+    const HandleCodeFile = async (path: string) => {
+        const response = await Post(`/api/v0/project/file`, {
+            filePath: path
+        });
+        console.log(response);
+
+        if (response.data.success) {
+            console.log(typeof response.data.data);
+
+            setCode(`${typeof response.data.data}`);
+        }
+    }
+
     return (
         <div className="flex-1 mr-4 my-4 rounded-xl border border-zinc-800 flex">
             <aside
                 className="bg-zinc-800/30 p-2 resize-x overflow-auto border-r border-zinc-800
-                                   min-w-[250px] max-w-[400px]">
+                                min-w-[250px] max-w-[400px]">
                 <div className="text-lg font-semibold mb-4 flex items-center gap-2 p-1 px-4">
                     <PiTreeStructureBold />
                     Files
                 </div>
-                <div className="flex flex-col">
-                    <div className="flex items-end cursor-pointer hover:bg-zinc-800/90 gap-1 p-1.5 rounded">
-                        <p className="text-lg"><FiChevronRight /></p>
-                        public
-                    </div>
-                    <div className="flex items-end cursor-pointer hover:bg-zinc-800/90 gap-1 p-1.5 rounded">
-                        <p className="text-lg"><FiChevronRight /></p>
-                        public
-                    </div>
-                </div>
+                {fileStructure && Array.isArray(fileStructure) ? (
+                    <FolderUI fileStructure={fileStructure} setFilePath={setFilePath} HandleCodeFile={HandleCodeFile} />
+                ) : (
+                    <p className="flex justify-center items-center gap-2">
+                        <span className="w-4 h-4 border-3 border-zinc-100 border-t-blue-700 rounded-full animate-spin"></span>
+                        Starting Live Preview
+                    </p>
+                )}
             </aside>
 
             <main className="flex-1 flex flex-col h-full">
                 <div className="text-sm flex gap-1 items-center text-zinc-400 font-semibold p-2 bg-zinc-900/90 border-b border-zinc-800">
-                    <p className="rounded hover:text-white cursor-pointer">src</p>
-                    <p className="text-lg"><FiChevronRight /></p>
-                    <p className="rounded hover:text-white cursor-pointer">index.ts</p>
+                    {!filePath && <p className="text-lg"><FiChevronRight /></p>}
+                    {filepathNav && filepathNav.map((path, index) => {
+                        return (
+                            <>
+                                <p key={index} className="rounded hover:text-white cursor-pointer">{path}</p>
+                                {filepathNav.length - 1 !== index && <p key={index} className="text-lg"><FiChevronRight /></p>}
+                            </>
+                        )
+                    })}
                 </div>
-                <div className="flex-1">
+                {code ? <div className="flex-1">
                     <Editor
                         height="100%"
                         defaultLanguage="javascript"
@@ -46,8 +77,16 @@ const FileStructure = () => {
                         options={{
                             readOnly: true,
                         }}
+
                     />
-                </div>
+                </div> :
+                    (
+                        <div className="flex justify-center gap-2 items-center h-full">
+                            <p>Preview loading</p>
+                            <span className="w-4 h-4 border-3 border-gray-300 border-t-blue-600 rounded-full animate-spin"></span>
+                        </div>
+                    )
+                }
             </main>
         </div>
     )
---
+++ frontend/src/components/PreviewWebsite.tsx
@@ -0,0 +1,9 @@
+const PreviewWebsite = () => {
+  return (
+    <div className='flex-1 rounded-xl mr-4 my-4'>
+      <iframe className='rounded' width="100%" height="100%" src="https://benji.app/"></iframe>
+    </div>
+  )
+}
+
+export default PreviewWebsite
